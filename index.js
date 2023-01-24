a70c02044a8c6f818c556e3cdab4a0ce2943896e
rokayaamjaad <Rukayaamged733@gmail.com>
2023-01-24 23:11:56 +0300

    f

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+// code your solution here;
+function saturdayFun(specified= "roller-skate") {
+
+ return `This Saturday, I want to ${specified}!`;
+}
+console.log(saturdayFun())
+console.log(saturdayFun(`bath my dog`));
+
+function mondayWork(defaultActivity ="go to the office") {
+
+    return `This Monday, I will ${defaultActivity}.`;
+
+}
+function wrapAdjective(String="*") {
+
+   return function(adj="special"){
+     return `You are ${String}${adj}${String}!`;
+   }
+}
+console.log(wrapAdjective("%")("a dedicated programmer"))
